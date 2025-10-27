89777acf50cca974f011d1e16e182afc0584833c
Igor Bossenko <igor.bossenko@gmail.com>
2025-10-27 14:56:49 +0200

    root folder renaming to support default GitPage build

--- _publish.sh
+++ _publish.sh
@@ -1,6 +1,6 @@
 java -jar ../publisher.jar -go-publish \
    -source    /Users/igor/source/hl7lt/ig-lt-base \
-   -web       /Users/igor/source/hl7lt/publication/webroot \
+   -web       /Users/igor/source/hl7lt/publication/docs \
    -history   /Users/igor/source/hl7lt/ig-history \
    -registry  /Users/igor/source/hl7lt/ig-registry/fhir-ig-list.json \
    -temp      /Users/igor/source/hl7lt/publication/temp \
